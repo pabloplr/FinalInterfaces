c9241fffcf0945d0166addb21c7ef020a8ec5a90
pabloplr <pablopezromero@gmail.com>
2022-02-06 20:48:44 +0100

    galeria terminada

--- js/golems.js
+++ js/golems.js
@@ -1,13 +1,4 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    $("#btn_flip_to_front").click(() => {
-        console.log("cambiando golem");
-        if ($("#btn_flip_to_front").innerHtml === 'ver estadisticas') {
-            $("#btn_flip_to_front").html('ver golem');
-            $("#card").flip(true);
-        } else {
-            $("#card").flip(false);
-            $("#btn_flip_to_front").html('ver estadisticas');
-        }
-    });
+
 });
